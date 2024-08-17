0cdb919c2536ddb9c52d870d15ddf8648ef67645
DenisseAlejandra28 <d.alejandra.clavel@gmail.com>
2024-08-17 01:57:29 +0000

    ultima clase

--- calificar_3.py
+++ calificar_3.py
@@ -1,12 +1,12 @@
 puntaje = int(input("ingresar puntos del 1 al 100: "))
 
-if 90 <= puntaje <= 100:
+if puntaje >= 90:
     print("calificacion: A")
-elif 80 <= puntaje <= 90:
+elif puntaje >= 80:
     print("calificacion: B")
-elif 70 <= puntaje <= 80:
+elif puntaje >= 70:
     print("calificacion: C")
-elif 60 <= puntaje <= 70:
+elif puntaje >= 60:
     print("calificacion: D")
 else:
     print("calificacion: F")
